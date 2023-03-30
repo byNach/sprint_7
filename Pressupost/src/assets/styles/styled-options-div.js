25240d1938c073de63ec9c490d536972dddfe5ce
nach <nachnitror@gmail.com>
2023-03-30 11:03:32 +0200

    ADD styled-div-options

---
+++ Pressupost/src/assets/styles/styled-options-div.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const OptionsDiv = styled.div`
+  background-color: antiquewhite;
+  border: 1px solid black;
+  border-radius: 40xp;
+`
+export default OptionsDiv

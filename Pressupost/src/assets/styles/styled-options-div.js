cc191c90aad779eb5277eaf1b4ab13f24f5b5db3
nach <nachnitror@gmail.com>
2023-04-01 10:18:25 +0200

    ADD styles on div options

--- Pressupost/src/assets/styles/styled-options-div.js
+++ Pressupost/src/assets/styles/styled-options-div.js
@@ -1,8 +1,15 @@
 import styled from 'styled-components';
 
-const OptionsDiv = styled.div`
-  background-color: antiquewhite;
+const OptionsGeneralDiv = styled.div`
+  margin: 20px;
+  width: 380px;
   border: 1px solid black;
-  border-radius: 40xp;
+  border-radius: 15px;
+  padding: 10px;
 `
-export default OptionsDiv
+
+const OptionsDiv = styled.div`
+margin: 5px;
+
+`
+export {OptionsGeneralDiv, OptionsDiv}
